acb3c59097dcf87353fe4695602015c8effef653
Ravagraid <levarielravagraiddemorial@gmail.com>
2023-11-09 13:03:55 +0000

    Added colorscheme, wiped maps

--- lua/Lazy.lua
+++ lua/Lazy.lua
@@ -15,99 +15,28 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({
-	"sainnhe/everforest",
-	"nvim-lualine/lualine.nvim",
-	{ "folke/which-key.nvim", event = "VeryLazy" },
-
-	"MattesGroeger/vim-bookmarks",
-	"tom-anders/telescope-vim-bookmarks.nvim",
-
-	"williamboman/mason.nvim",
-	"williamboman/mason-lspconfig.nvim",
-	"neovim/nvim-lspconfig",
-	"WhoIsSethDaniel/mason-tool-installer.nvim",
-
-	"rmagatti/alternate-toggler",
-	"windwp/nvim-autopairs",
-	"mg979/vim-visual-multi",
-	"gcmt/wildfire.vim",
-	{
-		"kylechui/nvim-surround",
-		version = "*", -- Use for stability; omit to use `main` branch for the latest features
-		event = "VeryLazy",
-		config = function()
-			require("nvim-surround").setup()
-		end,
+require("lazy").setup("plugins", {
+	defaults = {
+		lazy = true,
 	},
-
-	"nvim-telescope/telescope.nvim",
-
-	"hrsh7th/cmp-nvim-lsp",
-	"hrsh7th/cmp-buffer",
-	"hrsh7th/cmp-path",
-	"hrsh7th/cmp-cmdline",
-	"hrsh7th/nvim-cmp",
-	"onsails/lspkind-nvim",
-	{
-		"glepnir/lspsaga.nvim",
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter", -- optional
-			"nvim-tree/nvim-web-devicons", -- optional
-		},
+	ui = {
+		border = "rounded",
 	},
-	"L3MON4D3/LuaSnip",
-	"saadparwaiz1/cmp_luasnip",
-
-	"nvim-treesitter/nvim-treesitter",
-	"stevearc/conform.nvim",
-	"folke/todo-comments.nvim",
-	"numToStr/Comment.nvim",
-	"akinsho/toggleterm.nvim",
-
-	"f-person/git-blame.nvim",
-	{
-		"nvim-neo-tree/neo-tree.nvim",
-		branch = "v3.x",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-tree/nvim-web-devicons",
-			"MunifTanjim/nui.nvim",
-		},
+	install = {
+		colorscheme = { "everforest" },
 	},
-	"fedepujol/move.nvim",
-	"gbprod/yanky.nvim",
-	"mechatroner/rainbow_csv",
-
-	{
-		"folke/flash.nvim",
-		event = "VeryLazy",
-		opts = {},
-		keys = {
-			{
-				"s",
-				mode = { "n", "x", "o" },
-				function()
-					require("flash").jump()
-				end,
-				desc = "Flash",
-			},
-			{
-				"S",
-				mode = { "n", "o", "x" },
-				function()
-					require("flash").treesitter()
-				end,
-				desc = "Flash Treesitter",
+	performance = {
+		rtp = {
+			disabled_plugins = {
+				"gzip",
+				"matchit",
+				"matchparen",
+				"netrwPlugin",
+				"tarPlugin",
+				"tohtml",
+				"tutor",
+				"zipPlugin",
 			},
 		},
 	},
 })
-
-require("Coding")
-require("Colorscheme")
-require("Editor")
-require("Formatting")
-require("LSP")
-require("Treesitter")
-require("UI")
---
+++ lua/plugins/Colorscheme.lua
@@ -0,0 +1,41 @@
+return {
+	{
+    "sainnhe/everforest",
+    lazy = false,
+    priority = 1000,
+
+    config = function()
+      vim.cmd("colorscheme everforest")
+    end,
+  },
+}
+
+--vim.g.everforest_diagnostic_line_highlight = 1
+
+
+--vim.fn.sign_define({
+--	{
+--		name = "DiagnosticSignError",
+--		text = "",
+--		texthl = "DiagnosticSignError",
+--		linehl = "ErrorLine",
+--	},
+--	{
+--		name = "DiagnosticSignWarn",
+--		text = "",
+--		texthl = "DiagnosticSignWarn",
+--		linehl = "WarningLine",
+--	},
+--	{
+--		name = "DiagnosticSignInfo",
+---		text = "",
+--		texthl = "DiagnosticSignInfo",
+--		linehl = "InfoLine",
+--	},
+--	{
+--		name = "DiagnosticSignHint",
+--		text = "",
+--		texthl = "DiagnosticSignHint",
+--		linehl = "HintLine",
+--	},
+--})
