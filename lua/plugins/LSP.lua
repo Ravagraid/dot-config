2d1351f55efaba9539d535dfd985ffa6a745d70a
Ravagraid <levarielravagraiddemorial@gmail.com>
2023-11-09 13:26:38 +0000

    Added Mason

---
+++ lua/plugins/LSP.lua
@@ -0,0 +1,29 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+      "WhoIsSethDaniel/mason-tool-installer.nvim",
+    },
+  },
+  --Mason
+  {
+    	"williamboman/mason.nvim",
+      cmd = "Mason",
+      keys = {{"<leader>cm", "<cmd>Mason<cr>", desc = "Mason" }},
+      build = ":MasonUpdate",
+      opts = {
+        PATH = "prepend",
+      },
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    ensure_installed = {
+      "lua-language-server",
+      "stylua",
+      "vim-language-server",
+    },
+    auto_update = true,
+  },
+}
