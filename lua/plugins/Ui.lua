707f8a39533b08f82946a2a210153d06b351ebc0
Ravagraid <levarielravagraiddemorial@gmail.com>
2023-11-10 14:34:50 +0000

    floating terminal with lazygit

--- lua/plugins/Ui.lua
+++ lua/plugins/Ui.lua
@@ -1,49 +1,65 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	event = "VeryLazy",
-	init = function()
-		vim.g.lualine_laststatus = vim.o.laststatus
-		if vim.fn.argc(-1) > 0 then
-			-- set an empty statusline till lualine loads
-			vim.o.statusline = " "
-		else
-			-- hide the statusline on the starter page
-			vim.o.laststatus = 0
-		end
-	end,
-	opts = function()
-		vim.o.laststatus = vim.g.lualine_laststatus
-		return {
-			options = {
-				icons_enabled = true,
-				theme = "everforest",
-				component_separators = { left = "", right = "" },
-				section_separators = { left = "", right = "" },
-				always_divide_middle = true,
-				globalstatus = true,
-				refresh = {
-					statusline = 1000,
-					tabline = 1000,
-					winbar = 1000,
+	--Statusline
+	{
+		"nvim-lualine/lualine.nvim",
+		event = "VeryLazy",
+		init = function()
+			vim.g.lualine_laststatus = vim.o.laststatus
+			if vim.fn.argc(-1) > 0 then
+				-- set an empty statusline till lualine loads
+				vim.o.statusline = " "
+			else
+				-- hide the statusline on the starter page
+				vim.o.laststatus = 0
+			end
+		end,
+		opts = function()
+			vim.o.laststatus = vim.g.lualine_laststatus
+			return {
+				options = {
+					icons_enabled = true,
+					theme = "everforest",
+					component_separators = { left = "", right = "" },
+					section_separators = { left = "", right = "" },
+					always_divide_middle = true,
+					globalstatus = true,
+					refresh = {
+						statusline = 1000,
+						tabline = 1000,
+						winbar = 1000,
+					},
 				},
-			},
-			sections = {
-				lualine_a = { "mode" },
-				lualine_b = { { "filename", path = 1 } },
-				lualine_c = { "diagnostics", "diff", "filesize" },
-				lualine_x = { "encoding", "fileformat" },
-				lualine_y = { "location", "progress" },
-				lualine_z = { "filetype" },
-			},
-			lualine_c = { "filename" },
-			lualine_x = { "location" },
-		}
-	end,
+				sections = {
+					lualine_a = { "mode" },
+					lualine_b = { { "filename", path = 1 } },
+					lualine_c = { "diagnostics", "diff", "filesize" },
+					lualine_x = { "encoding", "fileformat" },
+					lualine_y = { "location", "progress" },
+					lualine_z = { "filetype" },
+				},
+				lualine_c = { "filename" },
+				lualine_x = { "location" },
+			}
+		end,
+	},
+
+	--Terminal
 	{
 		"akinsho/toggleterm.nvim",
-		opts = {
-			direction = "float",
-			open_mapping = [[<c-\>]],
-		},
+		opts = function()
+			function _lazygit_toggle()
+				require("toggleterm.terminal").Terminal:new({ cmd = "lazygit", hidden = true }):toggle()
+			end
+			return {
+				direction = "float",
+				open_mapping = [[<c-\>]],
+				vim.api.nvim_set_keymap(
+					"n",
+					"<leader>g",
+					"<cmd>lua _lazygit_toggle()<CR>",
+					{ noremap = true, silent = true }
+				),
+			}
+		end,
 	},
 }
