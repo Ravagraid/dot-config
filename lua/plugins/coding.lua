a45c0ecd786ff76272dcfb9264d87cdba876f8c7
Ravagraid <levarielravagraiddemorial@gmail.com>
2024-01-01 22:12:03 +0000

    perf(wrapping): removed Wrapping plugin

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -109,23 +109,4 @@ return {
 			"nvim-telescope/telescope.nvim",
 		},
 	},
-	{
-		"andrewferrier/wrapping.nvim",
-		opts = {
-			auto_set_mode_filetype_allowlist = {
-				"asciidoc",
-				"gitcommit",
-				"latex",
-				"mail",
-				"markdown",
-				"rst",
-				"tex",
-				"text",
-				"norg",
-			},
-		},
-		config = function()
-			require("wrapping").setup()
-		end,
-	},
 }
